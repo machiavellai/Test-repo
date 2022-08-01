5467bf1f4c42302351ae63306598388e4cce9134
machiavelli <oghenevictor54p@gmail.com>
2022-08-01 10:41:16 +0100

    "my initial message dawg"

---
+++ src/main/java/com/in28minutes/springboot/web/springbookfirstwebapplication/SpringBookFirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web.springbookfirstwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookFirstWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springboot/web/springbookfirstwebapplication/SpringBookFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.web.springbookfirstwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
